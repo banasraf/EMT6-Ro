4e7bba12872545ca98c4ec2874dd0bc2beacc37f
Rafał Banaś <Banas.Rafal97@gmail.com>
2019-08-11 18:47:11 +0200

    Diffusion implemented.

--- src/emt6ro/common/grid.h
+++ src/emt6ro/common/grid.h
@@ -1,13 +1,37 @@
 #ifndef SRC_EMT6RO_COMMON_GRID_H_
 #define SRC_EMT6RO_COMMON_GRID_H_
+#include <cstdint>
+#include <cuda_runtime.h>
 
 namespace emt6ro {
 
 struct Dims {
   uint32_t height;
   uint32_t width;
+
+  inline __host__ __device__ uint32_t vol() {
+    return height * width;
+  }
+};
+
+template <typename T>
+struct GridView {
+  T *data;
+  Dims dims;
+
+  inline __host__ __device__  const T& operator()(uint32_t r, uint32_t c) const {
+    return data[r * dims.width + c];
+  }
+
+  inline __host__ __device__ T& operator()(uint32_t r, uint32_t c) {
+    return data[r * dims.width + c];
+  }
 };
 
 }
+
+#define GRID_FOR(H, W) \
+for (uint32_t r = threadIdx.y + 1; r < H - 1; r += blockDim.y) \
+  for (uint32_t c = threadIdx.x + 1; c < W - 1; c += blockDim.x)
 
 #endif  // SRC_EMT6RO_COMMON_GRID_H_
---
+++ src/emt6ro/diffusion/grid-diffusion.h
@@ -0,0 +1,24 @@
+#ifndef SRC_EMT6RO_DIFFUSION_GRID_DIFFUSION_H
+#define SRC_EMT6RO_DIFFUSION_GRID_DIFFUSION_H
+
+#include <cuda_runtime.h>
+#include "emt6ro/site/site.h"
+#include "emt6ro/common/grid.h"
+
+namespace emt6ro {
+
+__device__ void diffuse(const GridView<float> &input, GridView<float> &output,
+                        float coeff, float time_step);
+
+void batchDiffuse(float *data, Dims dims, size_t batch_size,
+                  float coeff, float time_step, uint32_t steps);
+
+void copySubstrates(float *cho, float *ox, float *gi,
+                    const Site *sites, Dims dims, uint32_t batch_size);
+
+void copySubstratesBack(Site *sites, const float *cho, const float *ox, const float *gi,
+                        Dims dims, uint32_t batch_size);
+
+}  // namespace emt6ro
+
+#endif  // SRC_EMT6RO_DIFFUSION_GRID_DIFFUSION_H
