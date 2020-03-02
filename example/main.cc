fdfd13621705e410e6b9601f288b7ef86dfbfa92
Rafal Banas <Banas.Rafal97@gmail.com>
2020-03-02 17:21:03 +0100

    new timelapse

--- example/main.cc
+++ example/main.cc
@@ -59,6 +59,33 @@ class Timelapse {
   }
 };
 
+void add_to_timelapse(Simulation &simulation, Timelapse &timelapse, unsigned t) {
+  emt6ro::HostGrid<Site> state(Dims{53, 53});
+  for (uint32_t tumor = 0; tumor < 10; ++tumor) {
+    for (uint32_t test = 0; test < 100; ++test) {
+      simulation.getData(state.view().data, tumor * 100 + test);
+      auto view = state.view();
+      uint32_t phase_count[] = {0, 0, 0, 0, 0};
+      uint32_t alive_count = 0;
+      for (int32_t r = 0; r < view.dims.height; ++r) {
+        for (int32_t c = 0; c < view.dims.width; ++c) {
+          if (view(r, c).isOccupied()) {
+            ++alive_count;
+            ++phase_count[(uint8_t)view(r, c).cell.phase];
+          }
+        }
+      }
+      timelapse.val(tumor, t, test, Feature::TOTAL_CELLS) = alive_count;
+      timelapse.val(tumor, t, test, Feature::G1) = phase_count[0];
+      timelapse.val(tumor, t, test, Feature::S) = phase_count[1];
+      timelapse.val(tumor, t, test, Feature::G2M) = phase_count[2] + phase_count[3];
+      timelapse.val(tumor, t, test, Feature::CHO) = view(26, 26).substrates.cho;
+      timelapse.val(tumor, t, test, Feature::OX) = view(26, 26).substrates.ox;
+      timelapse.val(tumor, t, test, Feature::GI) = view(26, 26).substrates.gi;
+    }
+  }
+}
+
 static void experiment() {
   auto params = Parameters::loadFromJSONFile("../data/default-parameters.json");
   std::vector<emt6ro::HostGrid<Site>> states;
@@ -66,59 +93,46 @@ static void experiment() {
     std::cerr << "../data/tumor-lib/tumor-" + std::to_string(i) + ".txt" << std::endl;
     states.emplace_back(
         emt6ro::loadFromFile("../data/tumor-lib/tumor-" + std::to_string(i) + ".txt", params));
+//    auto state = states.back().view();
+//    for (uint32_t r = 1; r < 52; ++r) {
+//      for (uint32_t c = 1; c < 52; ++c) {
+//        if (state(r, c).isVacant()) {
+//          state(r, c).substrates.cho = params.external_levels.cho;
+//          state(r, c).substrates.ox = params.external_levels.ox;
+//        }
+//      }
+//    }
   }
-  auto state = emt6ro::loadFromFile("../data/tumor-lib/tumor-1.txt", params);
-  std::vector<float> protocol_data_h(5 * 24 * HOUR_STEPS / PROTOCOL_RES);  // 5 days protocol
-  protocol_data_h[0] = 1.25;
-  protocol_data_h[17 * HOUR_STEPS / PROTOCOL_RES] = 1.25;
-  protocol_data_h[34 * HOUR_STEPS / PROTOCOL_RES] = 1.25;
-  protocol_data_h[51 * HOUR_STEPS / PROTOCOL_RES] = 1.25;
-  protocol_data_h[68 * HOUR_STEPS / PROTOCOL_RES] = 1.25;
-  protocol_data_h[85 * HOUR_STEPS / PROTOCOL_RES] = 1.25;
-  protocol_data_h[103 * HOUR_STEPS / PROTOCOL_RES] = 1.25;
-  protocol_data_h[120 * HOUR_STEPS / PROTOCOL_RES] = 1.25;
 
-//  protocol_data_h[0] = 2;
-//  protocol_data_h[18 * HOUR_STEPS / PROTOCOL_RES] = 2;
-//  protocol_data_h[36 * HOUR_STEPS / PROTOCOL_RES] = 2;
-//  protocol_data_h[54 * HOUR_STEPS / PROTOCOL_RES] = 2;
-//  protocol_data_h[72 * HOUR_STEPS / PROTOCOL_RES] = 2;
+  std::vector<float> protocol_data_h(5 * 24 * HOUR_STEPS / PROTOCOL_RES + 1);  // 5 days protocol
+//  protocol_data_h[0] = 1.25;
+//  protocol_data_h[17 * HOUR_STEPS / PROTOCOL_RES] = 1.25;
+//  protocol_data_h[34 * HOUR_STEPS / PROTOCOL_RES] = 1.25;
+//  protocol_data_h[51 * HOUR_STEPS / PROTOCOL_RES] = 1.25;
+//  protocol_data_h[68 * HOUR_STEPS / PROTOCOL_RES] = 1.25;
+//  protocol_data_h[85 * HOUR_STEPS / PROTOCOL_RES] = 1.25;
+//  protocol_data_h[103 * HOUR_STEPS / PROTOCOL_RES] = 1.25;
+//  protocol_data_h[120 * HOUR_STEPS / PROTOCOL_RES] = 1.25;
+//
+  protocol_data_h[0] = 2;
+  protocol_data_h[18 * HOUR_STEPS / PROTOCOL_RES] = 2;
+  protocol_data_h[36 * HOUR_STEPS / PROTOCOL_RES] = 2;
+  protocol_data_h[54 * HOUR_STEPS / PROTOCOL_RES] = 2;
+  protocol_data_h[72 * HOUR_STEPS / PROTOCOL_RES] = 2;
   auto protocol_data =
-      buffer<float>::fromHost(protocol_data_h.data(), 5 * 24 * HOUR_STEPS / PROTOCOL_RES);
-  Protocol protocol{PROTOCOL_RES, SIM_LENGTH / 2, protocol_data.data()};
+      buffer<float>::fromHost(protocol_data_h.data(), 5 * 24 * HOUR_STEPS / PROTOCOL_RES + 1);
+  Protocol protocol{PROTOCOL_RES, SIM_LENGTH / 2 + 1, protocol_data.data()};
   std::random_device rd{};
   auto simulation = Simulation(BATCH_SIZE, params, rd());
   for (int i = 0; i < 10; ++i) {
     simulation.sendData(states[i], protocol, BATCH_SIZE/10);
   }
-  Timelapse timelapse(10, 960, 100);
-  for (int t = 0; t < 10 * 24 * 4; ++t) {
-    simulation.run(HOUR_STEPS / 4);
-    for (uint32_t tumor = 0; tumor < 10; ++tumor) {
-      for (uint32_t test = 0; test < 100; ++test) {
-        simulation.getData(state.view().data, tumor * 100 + test);
-        auto view = state.view();
-        uint32_t phase_count[] = {0, 0, 0, 0, 0};
-        uint32_t alive_count = 0;
-        for (int32_t r = 0; r < view.dims.height; ++r) {
-          for (int32_t c = 0; c < view.dims.width; ++c) {
-            if (view(r, c).isOccupied()) {
-              ++alive_count;
-              ++phase_count[(uint8_t)view(r, c).cell.phase];
-            }
-          }
-        }
-        timelapse.val(tumor, t, test, Feature::TOTAL_CELLS) = alive_count;
-        timelapse.val(tumor, t, test, Feature::G1) = phase_count[0];
-        timelapse.val(tumor, t, test, Feature::S) = phase_count[1];
-        timelapse.val(tumor, t, test, Feature::G2M) = phase_count[2] + phase_count[3];
-        timelapse.val(tumor, t, test, Feature::CHO) = view(27, 27).substrates.cho;
-        timelapse.val(tumor, t, test, Feature::OX) = view(27, 27).substrates.ox;
-        timelapse.val(tumor, t, test, Feature::GI) = view(27, 27).substrates.gi;
-      }
-    }
-    std::cout << std::endl;
-    std::cerr << (float)t * 0.25 << std::endl;
+  Timelapse timelapse(10, 10 * 24 * 60 + 1, 100);
+  add_to_timelapse(simulation, timelapse, 0);
+  for (int t = 0; t < 10 * 24 * 60; ++t) {
+    simulation.run(HOUR_STEPS / 60);
+    add_to_timelapse(simulation, timelapse, t+1);
+    if (t % 60 == 0) std::cerr << (float)(t/60) << std::endl;
   }
   for (unsigned tumor = 0; tumor < 10; ++tumor) {
     timelapse.dump_tumor(tumor, "tl2_outs/t" + std::to_string(tumor + 1) + ".txt");
