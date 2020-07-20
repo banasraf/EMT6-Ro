cbe3033df94f33db73795046e080720124c026ee
Wojciech Ozimek <ozimekwojciech@zoho.eu>
2020-07-20 23:48:19 +0200

    added random population generation (#23)

--- python_ga/utils.py
+++ python_ga/utils.py
@@ -3,10 +3,52 @@ import os
 import yaml
 
 
-def get_rand_population(population_size=120):
-    MIN_DOSE = 0.25
-    MAX_DOSE = 10
-    return np.clip(np.random.random((population_size, 21)) * 10, MIN_DOSE, MAX_DOSE)
+def get_random_protocol(
+    min_dose: float = 0.25,
+    max_dose: float = 10.0,
+    step_value: float = 0.25,
+    max_single_dose: float = 2.5,
+    time_steps: np.ndarray = None,
+):
+    protocol_dose = 0
+    max_protocol_value = max_single_dose
+    protocol_pair = []
+    while protocol_dose < max_dose:
+        if max_dose - protocol_dose < max_single_dose:
+            max_protocol_value = max_dose - protocol_dose
+
+        upper_value_boundary = int(round((max_protocol_value - min_dose) / step_value)) + 1
+        new_dose_value = np.random.randint(0, upper_value_boundary) * step_value + min_dose
+        protocol_pair.append(
+            (np.random.choice(time_steps), new_dose_value)
+        )
+        protocol_dose += new_dose_value
+    return protocol_pair
+
+
+def get_rand_population(
+        min_dose: float = 0.25,
+        max_dose: float = 10.0,
+        step_value: float = 0.25,
+        number_of_protocols: int = 10,
+        max_single_dose: float = 2.5,
+        hour_steps: int = 600,
+        protocol_resolution: int = 300,
+        available_hours: int = 24 * 5,
+):
+    time_steps = np.arange(0, available_hours * hour_steps, protocol_resolution)
+    protocol_pairs = []
+    for protocol in range(number_of_protocols):
+        protocol_pair = get_random_protocol(
+            min_dose=min_dose,
+            max_dose=max_dose,
+            step_value=step_value,
+            max_single_dose=max_single_dose,
+            time_steps=time_steps,
+        )
+        protocol_pairs.append(protocol_pair)
+
+    return protocol_pairs
 
 
 class ConvertRepresentation:
